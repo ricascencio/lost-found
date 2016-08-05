9ac2b9eadd2c742f045e17335dddb9338be424ec
Ricardo Ascencio <ricardo.ascencio@unosquare.com>
2016-08-05 17:54:51 -0500

    adding handlers

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -18,8 +18,8 @@ var OptionsBox = React.createClass({
   render: function() {
     return (
       <div className="optionsBox">
-      	<ButtonAction text="Perdi"/>
-        <ButtonAction text="Encontre"/>
+      	<ButtonAction text="Perdi"/>&nbsp;
+        <ButtonAction text="Encontre"/>&nbsp;
         <ButtonAction text="Buscar"/>
       </div>
     );
@@ -38,35 +38,116 @@ var ButtonAction = React.createClass({
 	}
 });
 
-var ObjectForm = React.createClass({
+var ObjectElement = React.createClass({
 	render: function(){
 		return (
+			<div className="object">
+				<h3>{this.props.name}</h3>
+				<p>
+					<DetailsObject/>
+				</p>
+			</div>
+		)
+	}
+});
+
+// var DetailsObject = React.createClass({
+// 	render: function(){
+// 		return (
+
+// 		)
+// 	}
+// )};
+
+var ObjectForm = React.createClass({
+	getInitialState: function(){
+		return {nombre:'', descripcion:'', ubicacion:'', objeto:'', owner:'',marca:'',color:''};
+	},
+	handleNombreChange: function(e){
+		this.state.nombre = e.target.value;
+	},
+	handleDescripcionChange: function(e){
+		this.state.descripcion = e.target.value;
+	},
+	handleUbicacionChange: function(e){
+		this.state.ubicacion = e.target.value;
+	},
+	handleObjetoChange: function(e){
+		this.state.objeto = e.target.value;
+	},
+	hanldeOwnerChange: function(e){
+		this.state.owner = e.target.value;
+	},
+	handleMarcaChange: function(e){
+		this.state.marca = e.target.value;
+	},
+	handleColorChange: function(e){
+		this.state.color = e.target.value;
+	},
+	handleAgregarObjeto: function(e){
+		var item = {
+					objeto:this.state.objeto
+				}
+
+		if(this.state.owner.split() !== ''){
+			console.log(this.state.owner);
+			item.concat()
+		}
+	}
+	handleSubmit: function(e){
+		e.preventDefault();
+		var item = {
+					nombre:this.state.nombre,
+					descripcion:this.state.descripcion,
+					ubicacion:this.state.ubicacion
+
+				};
+		console.log(item);
+	},
+	render: function(){
+		return (			
 			<div>
 				<h1>{this.props.typeAction}</h1>
 				<form className="objectForm">
-					<div>
-						<p>
-							<div >Objeto <input type="text" placeholder="nombre"/></div>
-							<div >Descripcion <input type="text" placeholder="descripcion"/></div>
-						</p>
-						<p>
-							<div>Ubicacion <input type="text" placeholder="donde lo perdiste/encontraste"/></div>
-						</p>
-						<p>
-						<label>Dinos que contiene</label>
-						</p>
-						<p>
-							<div >
-								Objeto <input type="text" placeholder="objeto"/>
-								Marca <input type="text" placeholder="marca"/>
+					<div className="rowDiv">
+						<div className="rowDiv">
+							<div className="leftDiv">Objeto <input type="text" placeholder="nombre" onChange={this.handleNombreChange}/></div>
+							<div className="rightDiv">Descripcion <input type="text" placeholder="descripcion"  onChange={this.handleDescripcionChange}/></div>
+						</div>
+						<div className="rowDiv">
+							<div>Ubicacion <input type="text" placeholder="donde lo perdiste/encontraste" size="40"  onChange={this.handleUbicacionChange}/></div>
+						</div>
+					</div>					
+					<p>
+						<h3>Dinos que contiene</h3>
+					</p>
+					<div className="objectsDiv">						
+						<div className="rowDiv">
+							Objeto <input type="text" placeholder="objeto"  onChange={this.handleObjetoChange}/>
+						</div>
+						<h3>Puedes agregar las siguientes propiedades</h3>
+						<div className="rowDiv">
+							<div>
+								Nombre/propietario <input type="text" placeholder="nombre" size="40" onChange={this.hanldeOwnerChange}/>&nbsp;														
 								<input type="button" value="+"/>
-							</div><br/>
-							<div >
-								Color <input type="text" placeholder="color"/>
-								<input type="button" value="+"/>
+								<input type="button" value="-" disabled="true"/>
 							</div>
-						</p>
+						</div>
+						<div className="rowDiv">
+							<div className="leftDiv">
+								Marca <input type="text" placeholder="marca" onChange={this.handleMarcaChange}/>&nbsp;
+								<input type="button" value="+"/>
+								<input type="button" value="-" disabled="true"/>
+							</div>
+							<div className="rightDiv">
+								Color <input type="text" placeholder="color"  onChange={this.handleColorChange}/>&nbsp;
+								<input type="button" value="+"/>
+								<input type="button" value="-" disabled="true"/>
+							</div>
+							<input type="button" value="Agregar objeto" onClick={this.handleAgregarObjeto}/>
+						</div>
 					</div>
+					<input type="button" value="Registrar" onClick={this.handleSubmit}/>
 				</form>
 			</div>
 		);
